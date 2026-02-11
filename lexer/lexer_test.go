a4d66af4a98ba3f541f101f3f8cbf2ae39ab8a62
dmonarchc <cisternasdelriodaniel@gmail.com>
2026-02-11 00:03:25 -0300

    Finish chapter 1

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -55,7 +55,10 @@ func TestNextToken2(t *testing.T) {
 							return true;
 						} else {
 						 	return false;
-						}`
+						}
+						10 == 10;
+						10 != 9;
+						`
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -125,6 +128,14 @@ func TestNextToken2(t *testing.T) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 	l := New(input)
